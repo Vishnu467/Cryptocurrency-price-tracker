983f57724245feabbc4c7c5bbade700352f327f3
Vishnu467 <71875089+Vishnu467@users.noreply.github.com>
2022-08-01 09:26:32 +0530

    Add files via upload

---
+++ src/main/java/com/example/cryptocurrency_price_tracker/CurrencyRVAdapter.java
@@ -0,0 +1,68 @@
+package com.example.cryptocurrency_price_tracker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.CurrencyViewholder> {
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+    private ArrayList<CurrencyModal> currencyModals;
+    private Context context;
+
+    public CurrencyRVAdapter(ArrayList<CurrencyModal> currencyModals, Context context) {
+        this.currencyModals = currencyModals;
+        this.context = context;
+    }
+
+    // method to filter our list.
+    public void filterList(ArrayList<CurrencyModal> filterllist) {
+        currencyModals = filterllist;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CurrencyRVAdapter.CurrencyViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // inflate the layout file
+        View view = LayoutInflater.from(context).inflate(R.layout.currency_rv_item, parent, false);
+        return new CurrencyRVAdapter.CurrencyViewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.CurrencyViewholder holder, int position) {
+        // setting data to our item of
+        // recycler view and its views.
+        CurrencyModal modal = currencyModals.get(position);
+        holder.nameTV.setText(modal.getName());
+        holder.rateTV.setText("$ " + df2.format(modal.getPrice()));
+        holder.symbolTV.setText(modal.getSymbol());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        //   returning the size of our array list.
+        return currencyModals.size();
+    }
+
+    //  creating our view holder class
+    public class CurrencyViewholder extends RecyclerView.ViewHolder {
+        private TextView symbolTV, rateTV, nameTV;
+
+        public CurrencyViewholder(@NonNull View itemView) {
+            super(itemView);
+            // initializing
+            symbolTV = itemView.findViewById(R.id.idTVSymbol);
+            rateTV = itemView.findViewById(R.id.idTVRate);
+            nameTV = itemView.findViewById(R.id.idTVName);
+        }
+    }
+}
